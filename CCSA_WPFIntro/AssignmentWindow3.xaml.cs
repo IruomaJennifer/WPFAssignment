07e08e23a2f02b1079a9e319a5990e7ed0f3075d
Iruoma Onyia <Iruomaonyia@outlook.com>
2022-08-25 21:10:15 +0100

    First commit

---
+++ CCSA_WPFIntro/AssignmentWindow3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CCSA_WPFIntro
+{
+    /// <summary>
+    /// Interaction logic for AssignmentWindow3.xaml
+    /// </summary>
+    public partial class AssignmentWindow3 : Window
+    {
+        public AssignmentWindow3()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CCSA_WPFIntro/AssignmentWindow5.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CCSA_WPFIntro
+{
+    /// <summary>
+    /// Interaction logic for AssignmentWindow5.xaml
+    /// </summary>
+    public partial class AssignmentWindow5 : Window
+    {
+        public AssignmentWindow5()
+        {
+            InitializeComponent();
+        }
+    }
+}
